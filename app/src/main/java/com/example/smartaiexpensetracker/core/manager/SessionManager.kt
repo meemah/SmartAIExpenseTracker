ce086c594b0c796ae498558902890443fdbd40e3
meemah <maimunahibro94@gmail.com>
2026-08-18 14:59:36 +0100

    feat: add session manager for user

--- app/src/main/java/com/example/smartaiexpensetracker/core/manager/SessionManager.kt
+++ app/src/main/java/com/example/smartaiexpensetracker/core/manager/SessionManager.kt
@@ -5,18 +5,22 @@ import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.sql.Ref
 import javax.inject.Inject
 import androidx.core.content.edit
+import com.example.smartaiexpensetracker.core.data.User
+import kotlinx.serialization.json.Json
 
 
-class TokenManager @Inject constructor (
+class SessionManager @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     companion object {
         const val ACCESS_TOKEN = "access_token"
         const val REFRESH_TOKEN = "refresh_token"
+
+        const val USER = "user"
     }
+
     private val prefs: SharedPreferences = EncryptedSharedPreferences.create(
         context,
         "secure_auth_prefs",
@@ -25,17 +29,32 @@ class TokenManager @Inject constructor (
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
 
-    fun getAccessToken() : String? = prefs.getString( ACCESS_TOKEN,null)
+    fun getAccessToken(): String? = prefs.getString(ACCESS_TOKEN, null)
 
-    fun getRefreshToken() : String? = prefs.getString(REFRESH_TOKEN,null)
+    fun getRefreshToken(): String? = prefs.getString(REFRESH_TOKEN, null)
 
-    fun clearTokens(){
+    fun clearSession() {
         prefs.edit { clear() }
     }
 
-    fun saveTokens(accessToken:String, refreshToken: String){
+    fun saveTokens(accessToken: String, refreshToken: String) {
         prefs.edit {
             putString(ACCESS_TOKEN, accessToken).putString(REFRESH_TOKEN, refreshToken)
+        }
+    }
+
+    fun saveUser(user: User) {
+        prefs.edit {
+            putString(USER, Json.encodeToString(user))
+        }
+    }
+
+    fun getUser(): User? {
+        val encodedUser = prefs.getString(USER, null) ?: return null
+        return try {
+            Json.decodeFromString<User>(encodedUser)
+        } catch (e: Exception) {
+            null
         }
     }
 }
